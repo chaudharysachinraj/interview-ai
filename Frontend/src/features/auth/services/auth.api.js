c30660b747972447f083ae4dcacc07dabf0c7ef2
chaudharysachinraj <rajchaudhary3801@gmail.com>
2026-05-03 03:06:11 +0530

    updated api url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-backend-im8u.onrender.com",
     withCredentials: true
 })
 
